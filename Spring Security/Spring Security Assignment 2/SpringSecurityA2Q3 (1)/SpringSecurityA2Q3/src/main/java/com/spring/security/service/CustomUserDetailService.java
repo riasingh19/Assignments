243a56068c1c2afeb8dc760fcde666441a896f89
Ria Singh <singh_ria.cs@ghrce.raisoni.net>
2022-03-07 20:42:10 +0530

    new

---
+++ Spring Security/Spring Security Assignment 2/SpringSecurityA2Q3 (1)/SpringSecurityA2Q3/src/main/java/com/spring/security/service/CustomUserDetailService.java
@@ -0,0 +1,32 @@
+package com.spring.security.service;
+
+
+import com.spring.security.model.CustomUserDetail;
+import com.spring.security.model.User;
+import com.spring.security.repository.UserRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = this.userRepo.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("INVALID USERNAME!!");
+        }
+
+        return new CustomUserDetail(user);
+    }
+
+}
